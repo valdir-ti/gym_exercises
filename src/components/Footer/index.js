e29932203525aff71dc51b121dcf486d5ddda31f
Valdir Silva <valdir.ti@gmail.com>
2022-06-18 15:16:18 -0300

    feat: add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,35 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from "../../assets/images/Logo-1.png"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box
+      mt="80px"
+      bgcolor="#FFF3F4"
+    >
+      <Stack
+        gap="40px"
+        alignItems="center"
+        px="40px"
+        pt="40px"
+      >
+        <img
+          src={Logo}
+          alt="Logo"
+          width="200px"
+          height="40px"
+        />
+        <Typography
+          variant="h5"
+          pb="40px"
+          mt="20px"
+        >
+          Made with ❤️ by Valdir Silva
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
